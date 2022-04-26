3b0dd6842945abde3c9e0b95f7ab5653b1958bf1
HACKER <76677018+HackerTHINS@users.noreply.github.com>
2022-04-26 20:46:20 +0700

    Add files via upload
    SHARES BOR HACK

---
+++ Bot.sh
@@ -0,0 +1,341 @@
+red='\33[33;1m'
+bluelight='\33[36;1m'
+w="\033[00m"
+r="\033[31;1m"
+g="\033[32;1m"
+y="\033[33;1m"
+b="\033[34;1m"
+p="\033[35;1m"
+c="\033[36;1m"
+W="\033[47m"
+R="\033[41m"
+B="\033[0;30m"
+clear
+echo $red "Loading..."
+sleep 2
+echo "SHARES AUTOMATIC SEND"
+sleep 4
+clear
+echo $red"   █▀▀▀█ ░█─░█ ─█▀▀█ ░█▀▀█ ░█▀▀▀ ░█▀▀▀█"
+echo $red"  ─▀▀▀▄▄ ░█▀▀█ ░█▄▄█ ░█▄▄▀ ░█▀▀▀ ─▀▀▀▄▄"
+echo $red"   █▄▄▄█ ░█─░█ ░█─░█ ░█─░█ ░█▄▄▄ ░█▄▄▄█"
+echo
+read -p "URL : " Url
+sleep 3
+read -p "THEARDS : " THRD
+sleep 3
+echo
+echo $bluelight "SCANNING URL..."
+sleep 2
+clear
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $g "Shares bot has been sent successfully[✓]"
+sleep 1
+echo $red "THANK YOU FOR USING SHARES BOT TIKTOK"
+sleep 3
+echo "Check Your tiktok video"
+sleep 10
+
